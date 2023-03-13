e47d49b6c88001b2d0328c3eb2a4860ccc19b811
Paolobaiada <paolosettemaggio@gmail.com>
2023-03-13 16:05:32 +0100

    static

--- es-60/exercise-3.js
+++ es-60/exercise-3.js
@@ -10,10 +10,10 @@ class Person {
   }
 }
 
-const obj = {
+ const obj = {
   firstName: "Mario",
   lastName: "Rossi",
-};
+}; 
 
 const person = Person.fromObject(obj);
 console.log(person.firstName + " " + person.lastName);
