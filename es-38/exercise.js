b5d4d6bcf8aa98c9cc0f652df8c124ef280e4d5b
Paolobaiada <paolosettemaggio@gmail.com>
2023-03-13 09:16:12 +0100

    merge

--- es-38/exercise.js
+++ es-38/exercise.js
@@ -2,11 +2,13 @@ function printName(){
     let helloName = "Hello Jonh";
 
 function inner(){
+   
    return helloName;
              }
              return inner()
+           
 }
-setTimeout(() =>{
+
     console.log(printName());
-},1000)
+
  
