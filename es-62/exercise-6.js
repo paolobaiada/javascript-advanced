d37f42f8c4b6690e94df9a10231c0d9f5dc4c7af
PaoloBaiada <paolosettemaggio@gmail.com>
2023-03-17 14:41:56 +0100

    calculate

---
+++ es-62/exercise-6.js
@@ -0,0 +1,43 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+  area(){
+    return this.side * this.side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  area(){
+    return this.width * this.height
+  }
+ 
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  area(){
+    return Math.PI * this.radius * this.radius
+  }
+}
+
+class AreaCalculator {
+static calculate(figure){
+  return figure.area();
+
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+ console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle)); 
