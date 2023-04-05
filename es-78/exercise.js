15f5e20f4c2080b55ec24f44eb742cc2be820c2d
PaoloBaiada <paolosettemaggio@gmail.com>
2023-04-05 11:19:30 +0200

    setintervall

--- es-78/exercise.js
+++ es-78/exercise.js
@@ -1,7 +1,7 @@
 function repeatHello(callback){ 
     
-let id = setInterval(() => console.log(callback),1000);
-  setTimeout(() =>  clearInterval(id), 5000);
+ setInterval(() => console.log(callback),1000);
+
 }
 function hello(){
     return "Hello"
